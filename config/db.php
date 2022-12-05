39eaacab4b692f067b298bc2fc204ff6118c9e03
anderbolado98 <114226274+anderbolado98@users.noreply.github.com>
2022-12-05 08:37:10 +0100

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+	/*-------------------------
+	Autor: Obed Alvarado
+	Web: obedalvarado.pw
+	Mail: info@obedalvarado.pw
+	---------------------------*/
+define("DB_HOST", "192.168.1.37");//DB_HOST:  generalmente suele ser "127.0.0.1"
+define("DB_NAME", "ciberpepes_db");//Nombre de la base de datos
+define("DB_USER", "ciberpepes");//Usuario de tu base de datos
+define("DB_PASS", "CIBER1234");//Contraseña del usuario de la base de datos
+
